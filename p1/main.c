2e63580f66937ddea4a7075fbb4e8812bf1ab792
2025050125 <codnjs486012@kau.kr>
2026-05-27 15:25:41 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    char arr[100] = {}; 
-    int N = 10; 
-
-    char *p = NULL; 
-
-    for (p = arr; p < arr + N; p++) {
-        scanf(" %c", p); 
+    char alpha[10] = {}; 
+    
+    for (int i = 0; i < 10; i++) {
+        scanf(" %c", &alpha[i]); 
     }
 
-    char max_char = *arr; 
+    char max_char = alpha[0];
     int max_count = 0;
 
-    for (p = arr; p < arr + N; p++) {
+    for (char *p = alpha; p < alpha + 10; p++) {
         int current_count = 0;
         
-        for (char *q = arr; q < arr + N; q++) {
+        for (char *q = alpha; q < alpha + 10; q++) {
             if (*p == *q) {
                 current_count++;
             }
         }
         
+
         if (current_count > max_count) {
             max_count = current_count;
             max_char = *p;
         }
     }
-
+    
     printf("%c%d\n", max_char, max_count);
 
     return 0;
